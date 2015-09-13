249348e11c40a854a9d8e1d46ca0457035d17ede
buswedg <darrylbuswell@gmail.com>
2015-09-13 16:20:44 +0800

    Added: QwTSQL M3

---
+++ Querying with Transact-SQL/Lab3 Querying Multiple Tables with Joins/2 Retrieve Sales Data.sql
@@ -0,0 +1,27 @@
+-- Retrieve a list of all customers and their orders
+SELECT c.CompanyName, c.FirstName, c.LastName, oh.SalesOrderID, oh.TotalDue
+FROM SalesLT.Customer AS c
+LEFT JOIN SalesLT.SalesOrderHeader AS oh
+ON c.CustomerID = oh.CustomerID
+ORDER BY oh.SalesOrderID DESC;
+
+
+-- Retrieve a list of customers with no address
+SELECT c.CompanyName, c.FirstName, c.LastName, c.Phone
+FROM SalesLT.Customer AS c
+LEFT JOIN SalesLT.CustomerAddress AS ca
+ON c.CustomerID = ca.CustomerID
+WHERE ca.AddressID IS NULL;
+
+
+-- Retrieve a list of customers and products without orders
+SELECT c.CustomerID, p.ProductID
+FROM SalesLT.Customer AS c
+FULL JOIN SalesLT.SalesOrderHeader AS oh
+ON c.CustomerID = oh.CustomerID
+FULL JOIN SalesLT.SalesOrderDetail AS od
+ON od.SalesOrderID = oh.SalesOrderID
+FULL JOIN SalesLT.Product AS p
+ON p.ProductID = od.ProductID
+WHERE oh.SalesOrderID IS NULL
+ORDER BY ProductID, CustomerID;
