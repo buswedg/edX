58670ba9dd4c2005cf4e77361949c1cca1512fb8
buswedg <darrylbuswell@gmail.com>
2015-09-15 18:02:07 +0800

    Added: QwTSQL M7

---
+++ Querying with Transact-SQL/Lab7 Using Table Expressions/1 Retrieve Product Information.sql
@@ -0,0 +1,27 @@
+-- Retrieve product model descriptions
+SELECT P.ProductID, P.Name AS ProductName, PM.Name AS ProductModel, PM.Summary
+FROM SalesLT.Product AS P
+JOIN SalesLT.vProductModelCatalogDescription AS PM
+ON P.ProductModelID = PM.ProductModelID
+ORDER BY ProductID;
+
+
+-- Create a table of distinct colors
+DECLARE @colors AS TABLE (Color nvarchar(15));
+
+INSERT INTO @Colors
+SELECT DISTINCT Color FROM SalesLT.Product;
+
+SELECT ProductID, Name, Color
+FROM SalesLT.Product
+WHERE Color IN (SELECT Color FROM @Colors);
+
+
+-- Retrieve product parent categories
+SELECT C.ParentProductCategoryName AS ParentCategory,
+       C.ProductCategoryName AS Category,
+       P.ProductID, P.Name AS ProductName
+FROM SalesLT.Product AS P
+JOIN dbo.ufnGetAllCategories() AS C
+ON P.ProductCategoryID = C.ProductCategoryID
+ORDER BY ParentCategory, Category, ProductName;
---
+++ Querying with Transact-SQL/Lab7 Using Table Expressions/2 Retrieve Customer Sales Revenue.sql
@@ -0,0 +1,22 @@
+-- Retrieve sales revenue by customer and contact (derived table)
+SELECT CompanyContact, SUM(SalesAmount) AS Revenue
+FROM
+	(SELECT CONCAT(c.CompanyName, CONCAT(' (' + c.FirstName + ' ', c.LastName + ')')), SOH.TotalDue
+	 FROM SalesLT.SalesOrderHeader AS SOH
+	 JOIN SalesLT.Customer AS c
+	 ON SOH.CustomerID = c.CustomerID) AS CustomerSales(CompanyContact, SalesAmount)
+GROUP BY CompanyContact
+ORDER BY CompanyContact;
+
+
+-- Retrieve sales revenue by customer and contact (CTE)
+WITH CustomerSales(CompanyContact, SalesAmount)
+AS
+(SELECT CONCAT(c.CompanyName, CONCAT(' (' + c.FirstName + ' ', c.LastName + ')')), SOH.TotalDue
+ FROM SalesLT.SalesOrderHeader AS SOH
+ JOIN SalesLT.Customer AS c
+ ON SOH.CustomerID = c.CustomerID)
+SELECT CompanyContact, SUM(SalesAmount) AS Revenue
+FROM CustomerSales
+GROUP BY CompanyContact
+ORDER BY CompanyContact;
